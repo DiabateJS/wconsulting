fe31872b698d69fb7a3f5a26783e571c0164bf21
DiabateJS <diabatejs@yahoo.fr>
2022-06-11 21:38:07 +0200

    Suppression commentaires.

--- Modules/Home/controler.Home.php
+++ Modules/Home/controler.Home.php
@@ -1,26 +1,23 @@
-
-<?php 
- switch ($Action) {
-    case 'Home':
-        Home();
-        break;
-    
-    default:
-        # code...
-        break;
-    }
-/*if (isset($_SESSION['Id_agent'])) 
-{
-switch ($Action) {
-    case 'Home':
-        Home();
-        break;
-    
-    default:
-        # code...
-        break;
-    }
-}else {
-    Login();
-}*/
+
+
+<?php 
+
+ switch ($Action) {
+
+    case 'Home':
+
+        Home();
+
+        break;
+
+    
+
+    default:
+
+        # code...
+
+        break;
+
+    }
+
 ?>
